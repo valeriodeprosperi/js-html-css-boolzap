c5116c8ad0c9e8923991cf181904f31cb869222b
valerio <valerio.deprosperi@gmail.com>
2020-09-09 19:21:58 +0200

    modifiche css e js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,39 @@
 $(document).ready(function() {
-  
+  $(document).ready(function() {
+
+  $(".send-message").click(
+    function() {
+      sendMessage();
+    }
+  );
+
+  $("#input-message").keyup(
+    function(event) {
+      if(event.which == 13) {
+        sendMessage();
+      }
+    }
+  );
+
+});
+
+function sendMessage() {
+  var inputText = $("#input-message").val();
+
+  if(inputText != "") {
+    var templateMessage = $(".templates .message-row").clone();
+
+    var date = new Date();
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var time = hours + ":" + minutes;
+
+    templateMessage.find(".message-text").text(inputText);
+    templateMessage.find(".message-time").text(time);
+    templateMessage.addClass("sent");
+
+    $(".chat").append(templateMessage);
+    $("#input-message").val("");
+  }
 }
+};
